dae08fb7d9830dd6ff5f12a8d2eebf97a3502b40
zln1 <57205981+zln1@users.noreply.github.com>
2019-11-19 16:36:13 +0800

    add task8 11

---
+++ css_task9/js/task8.js
@@ -0,0 +1,7 @@
+function myfunction(){
+    if(document.getElementById("my-ul").style.display=="none"){
+        document.getElementById("my-ul").style.display="block";
+    }else {
+        document.getElementById("my-ul").style.display="none";
+    }
+}
